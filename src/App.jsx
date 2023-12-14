b91de4400dd29abacfe0f79ab535257688cce25e
Bikramjeet singh <bs863313@gmail.com>
2023-12-14 18:12:36 +0530

    Todo list working Start 1 basic design ready 2 add and showing  item into list

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import React from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState("");
+  const [task, settask] = useState([]);
+
+  function Addtask() {
+    settask([...task, count]);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <div className="main-todo">
+          <div className="">
+            <input type="text" onChange={(e) => setCount(e.target.value)} />
+            <button onClick={Addtask}>Add</button>
+          </div>
+
+          <div>
+            <h4>List of all Todos</h4>
+            <ul>
+              {task.map((item, index) => (
+                <li key={index}> {item}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
