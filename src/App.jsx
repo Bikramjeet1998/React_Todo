2df161fc22a2b5e4c2757a7b4a0c3401eb73fd97
Bikramjeet singh <bs863313@gmail.com>
2023-12-22 22:04:13 +0530

    Todo done with Use Reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,11 @@
-import { useState } from "react";
 import React from "react";
 import "./App.css";
+import Todo from "./components/Todo";
 
 function App() {
-  const [count, setCount] = useState("");
-  const [task, settask] = useState([]);
-
-  function Addtask() {
-    settask([...task, count]);
-  }
-
   return (
     <>
-      <div className="container">
-        <div className="main-todo">
-          <div className="">
-            <input type="text" onChange={(e) => setCount(e.target.value)} />
-            <button onClick={Addtask}>Add</button>
-          </div>
-
-          <div>
-            <h4>List of all Todos</h4>
-            <ul>
-              {task.map((item, index) => (
-                <li key={index}> {item}
-                
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      </div>
+      <Todo />
     </>
   );
 }
---
+++ src/components/Todo.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import { useReducer } from "react";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "INPUT":
+      return {
+        ...state,
+        inputValue: action.payload,
+      };
+    case "ADD":
+      return {
+        task: [...state.task, { id: Date.now(), title: state.inputValue }],
+        inputValue: "",
+      };
+    case "DELETE":
+      return {
+        ...state,
+        task: state.task.filter((item) => item.id !== action.payload),
+      };
+
+    default:
+      return state;
+  }
+};
+
+const initialState = {
+  inputValue: "",
+  task: [],
+};
+
+export default function Todo() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  function handle_delete(id) {
+    dispatch({
+      type: "DELETE",
+      payload: id,
+    });
+  }
+  console.log(state);
+  return (
+    <>
+      <div className="container">
+        <div className="main-todo">
+          <div className="">
+            <input
+              type="text"
+              value={state.inputValue}
+              onChange={(e) =>
+                dispatch({
+                  type: "INPUT",
+                  payload: e.target.value,
+                })
+              }
+            />
+            <button onClick={() => dispatch({ type: "ADD" })}>ADD</button>
+          </div>
+
+          <div>
+            <h4>List of all Todos</h4>
+            <ul>
+              {state.task.map((item, index) => {
+                return (
+                  <li key={index}>
+                    {item.title}
+                    <button onClick={() => handle_delete(item.id)}>
+                      delete
+                    </button>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
